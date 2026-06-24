7c5cfa9b4bda2d39c159ddce90877345acb5e2a0
NetRunner <128007180+net-learner-04@users.noreply.github.com>
2026-06-24 23:44:58 +0900

    Implement Users and Files models in SQLAlchemy

---
+++ models.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Boolean
+from db import Base
+from datetime import datetime
+
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, nullable=False, unique=True)
+    passwd = Column(String, nullable=False)
+    email = Column(String, unique=True)
+
+
+class Files(Base):
+    __tablename__ = "files"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    file_name = Column(String(30), nullable=False)
+    file_path = Column(String, nullable=False)
+    file_size = Column(Integer, nullable=False)
+    file_owner = Column(Integer, ForeignKey("users.id"), nullable=False)
+    file_type = Column(String(50))
+    is_deleted = Column(Boolean, default=False)
+    create_date = Column(DateTime, nullable=False, default=datetime.now)
