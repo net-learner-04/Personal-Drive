741db1b7de98079d2c6e47cb3ffd35553e9edd5c
NetRunner <128007180+net-learner-04@users.noreply.github.com>
2026-06-24 23:42:37 +0900

    Integrate user and file routers into main application

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI, Depends
-from fastapi.staticfiles import StaticFiles
-from sqlalchemy.orm import Session
-from db import engine, Base, SessionLocal
+from fastapi import FastAPI
+from domain.user import user_router
+from process import router as file_router
+from db import Base, engine
 
 
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.include_router(user_router.router)
+app.include_router(file_router)
